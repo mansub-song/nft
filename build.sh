99536be8d8c3f14cb915a670051703c07b0784bd
Mansub Song <ssongman1029@gmail.com>
2022-06-20 19:24:46 +0900

    Add .env file

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-npm install dotenv
+if [ $1 == "first" ]; then
+	npm install dotenv
 
-npm install @openzeppelin/contract
+	npm install @openzeppelin/contracts
 
-npx hardhat compile
+	npx hardhat compile
 
-npm install @alch/alchemy-web3
+	npm install @alch/alchemy-web3
 
-s
+# nft smart-contrat deploy 
+	npx hardhat run scripts/nft-deploy.js --network rinkeby
+else
+	 npx hardhat run scripts/nft-deploy.js --network rinkeby
+fi
--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-
-npx hardhat run scripts/nft-deploy.js --network rinkeby
-
 node scripts/mint-nft.js
